d04e1094b83efa90e310dc4c8f7efce2f8c16351
i-stam <i.stamelakos@gmail.com>
2020-07-21 23:26:13 +0100

    Upgrade to solc 0.6.11

--- contracts/mocks/walletMock.sol
+++ contracts/mocks/walletMock.sol
@@ -1,16 +1,18 @@
-pragma solidity 0.5.17;
+// SPDX-License-Identifier: GPLv3
+
+pragma solidity ^0.6.11;
 
 
 contract WalletMock {
     /// @dev Ether can be deposited from any source, so this contract must be payable by anyone.
-    function() external payable {}
+    receive() external payable {}
 
     function transfer(address payable _to, uint256 _amount) external {
         _to.transfer(_amount);
     }
 
     function sendValue(address payable _to, uint256 _amount) external {
-        (bool success, ) = _to.call.value(_amount)("");
+        (bool success, ) = _to.call{value: _amount }("");
         require(success, "sendValue failed");
     }
 }
